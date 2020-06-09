d6097198fdda9e04150c2f1e96bebc9972edba54
Colin Johnson <colin@colinjohnson.net>
2020-06-09 02:51:49 -0700

    do more course

--- run_scraper.sh
+++ run_scraper.sh
@@ -1,6 +1,6 @@
 # the term to scrape data for
 # this should always be set to whatever the "current" term people are resgistering for is.
-export TERM="202005"
+export TERM="202009"
 
 # activate python virtual environment (env is assumed to exist and have all requirements)
 source env/bin/activate
