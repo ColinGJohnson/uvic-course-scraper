8a5b09f40ef64b0c63858f4c6b52879b9b106685
Ubuntu <ubuntu@ip-172-31-22-16.us-west-2.compute.internal>
2020-06-14 18:46:20 +0000

    Add redirection to log file to runner script

--- run_scraper.sh
+++ run_scraper.sh
@@ -1,6 +1,7 @@
 # the term to scrape data for
 # this should always be set to whatever the "current" term people are resgistering for is.
 export TERM="202005"
+export LOGFILE="~/log.txt"
 
 # make sure we are in the right directory
 cd ~/uvic-course-scraper
@@ -14,16 +15,16 @@ rm -rf html/ json/
 
 # scrape search result pages
 echo "Scraping $TERM..."
-python3 ./Scraper.py --term=$TERM
+python3 ./Scraper.py --term=$TERM >> $LOGFILE
 
 # parse the search result pages into more useful json
 echo "Parsing $TERM..."
-python3 ./Parser.py 
+python3 ./Parser.py  >> $LOGFILE
 
 # upload changes to mongodb MONGODB_STR should be set in system environment variables
 for filename in ./json/*.json; do
     echo "Uploading $TERM..."
-    python3 ./Uploader.py --input="./json/$TERM.json" --constr="$MONGODB_STR"
+    python3 ./Uploader.py --input="./json/$TERM.json" --constr="$MONGODB_STR" >> $LOGFILE
 done
 
 echo "Course scraping complete!"
