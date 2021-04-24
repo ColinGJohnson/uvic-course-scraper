afd3939e782f6b79acb25529ad368c0fcf4a2bed
Colin Johnson <colin@colinjohnson.net>
2021-04-24 04:22:55 -0700

    Update course uploader

--- Uploader.py
+++ Uploader.py
@@ -1,90 +1,164 @@
 import time
 import datetime
-
-import pymongo
-import argparse
-from pprint import pprint
-
 import json
+import argparse
+import os
 
-def timestamp():
-	return datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+from dotenv import load_dotenv
+from requests_aws4auth import AWS4Auth
+import requests
+
+# parse arguments and load environment variables
+parser = argparse.ArgumentParser(
+    description="Upload course data to DynamoDB."
+)
+
+parser.add_argument("--input", 
+    help="JSON file which with to update the course documents.",
+    type=str,
+    required=True
+)
+
+args = parser.parse_args()
+load_dotenv()
+print(os.getenv('APPSYNC_URL'))
+
+# Use AWS4Auth to sign a requests session 
+# (see: https://pypi.org/project/requests-aws4auth/)
+# (see: https://stackoverflow.com/questions/60293311/how-to-send-a-graphql-query-to-appsync-from-python)
+session = requests.Session()
+session.auth = AWS4Auth(
+    os.getenv('AWS_ACCESS_KEY_ID'),
+    os.getenv('AWS_SECRET_KEY'),
+    'us-west-2',
+    'appsync'
+)
+
+# found in AWS Appsync under Settings for the course scheduler endpoint.
+APPSYNC_API_ENDPOINT_URL = os.getenv('APPSYNC_URL')
 
 
-def main():
+def send_graphql_query(session, query):
+    response = session.request(
+        url=APPSYNC_API_ENDPOINT_URL,
+        method='POST',
+        json={'query': check_course_existance_query}
+    )
 
-	parser = argparse.ArgumentParser(
-		description="Upload scraped course data to a mongodb cluster."
-	)
+    if response.status_code != 200:
+        raise Exception(f"Query failed, response code: {request.status_code}.")
+        return None
 
-	parser.add_argument("--input", 
-		help="JSON file which with to update the course documents.",
-		type=str,
-		required=True
-	)
+    return response.json()
 
-	parser.add_argument("--constr",
-		help=("MongoDB connection string to connect to the database with."),
-		type=str,
-		required=True
-	)
 
-	parser.add_argument("--database",
-		help=("Database to use."),
-		type=str,
-		default="schedulerdb"
-	)
+def check_course_exists(session, term, subject, course_code):
+    check_course_existance_query = f"""
+    query MyQuery {{
+        coursesByCode(
+            subjectCourse_code: {{
+                beginsWith: {{
+                    course_code: "{course_code}", 
+                    subject: "{subject}"
+                }}
+            }}, 
+            term: "{term}"
+        ) {{
+            items {{
+                id
+                course_code
+                term
+                subject
+            }}
+        }}
+    }}
+    """
 
-	parser.add_argument("--collection",
-		help=("Collection to use."),
-		type=str,
-		default="courses"
-	)
+    response = send_graphql_query(session, check_course_existance_query)
+    items = response['data']['coursesByCode']['items']
+    
+    if len(items) > 1:
+        raise Exception(f"Duplicate course found with query: {check_course_existance_query}")
 
-	args = parser.parse_args()
+    return None if len(items) == 0 else items[0]['id']
 
-	# read the specified file as json
-	with open(args.input, 'r') as file:
-		try:
-			jsonstr = file.read()
-		except Exception as e:
-			print("Error while reading input file: ", e)
 
-	data = json.loads(jsonstr)
-	print(f'Found {len(data)} items in input file.')
+def dict_to_graphql_mutation_input(object, level=0):
+        fields = []
+        for key in object.keys():
 
-	# connect to MongoDB
-	client = pymongo.MongoClient(args.constr)
-	database = client[args.database]
-	collection = database[args.collection]
+            if (type(object[key]) is list):
+                list_fields = []
+                for item in object[key]:
+                    list_fields.append(dict_to_graphql_mutation_input(item, level=level+1))
+                fields.append(f'{key}: [{", ".join(list_fields)}]')
 
-	# iterate over all the courses in the input file
-	updated = 0
-	inserted = 0
+            elif(type(object[key]) is str):
+                value = object[key].replace('\n', ' ')
+                fields.append(f'{key}: "{value}"')
 
-	for course in data:
+            else:
+                raise Exception(f"This function only works for str and list, not {type(object[key])}")
 
-		# find the course to update by term and crn
-		query = { 
-			'course_title': course['course_title'],
-			'subject': course['subject'],
-			'course_code': course['course_code'],
-			'term': course['term']
-		}
+        result = f"{{{' '.join(fields)}}}"
+        return f"input: {result}" if level == 0 else result
 
-		# create/update the course listing
-		result = collection.update_one(query, { '$set': course }, upsert=True)
 
-		if result.upserted_id is not None:
-			print(f'[{timestamp()}] inserted {course["term"]} {course["subject"]}-{course["course_code"]}: {course["course_title"]}')
-			inserted += 1
+# read the specified file as json
+with open(args.input, 'r') as file:
+    try:
+        jsonstr = file.read()
+    except Exception as e:
+        print("Error while reading input file: ", e)
+data = json.loads(jsonstr)
+print(f'Found {len(data)} items in input file.')
 
-		elif result.modified_count == 1:
-			print(f'[{timestamp()}] updated {course["term"]} {course["subject"]}-{course["course_code"]}: {course["course_title"]}: ')
-			updated += 1
+# iterate over all the courses in the input file
+updated = 0
+inserted = 0
+for course in data:
 
-	print(f'SUMMARY: updated {updated}, inserted {inserted}')
+    # check if the course already exists in the database
+    id = check_course_exists(session, course['term'], course['subject'], course['course_code'])
 
-# if this file isn't being imported, run automatically
-if __name__ == "__main__":
-	main()
+    # update the existing course if it exists, otherwise create it
+    mutation = ""
+    if id:
+        print(f"found existing course with id: {id}")
+        course['id'] = id
+        mutation = f"""
+        mutation UpdateCourse {{
+            updateCourse({dict_to_graphql_mutation_input(course)}) 
+            {{
+                id
+            }}
+        }}
+        """
+        updated += 1
+        
+    else:
+        print(f"no course found, creating new database entry")
+        mutation = f"""
+        mutation CreateCourse {{
+            createCourse({dict_to_graphql_mutation_input(course)}) 
+            {{
+                id
+            }}
+        }}
+        """
+        inserted += 1
+
+    # make the mutation
+    # response = session.request(
+    #     url=APPSYNC_API_ENDPOINT_URL,
+    #     method='POST',
+    #     json={'query': mutation}
+    # )
+
+    # print a summary of the changes
+    action = "Updated" if id else "Created"
+    print(f'{action} {course["term"]} {course["subject"]}-{course["course_code"]}: {course["course_title"]}: ')
+
+    break
+
+print(f'SUMMARY: updated {updated}, inserted {inserted}')
--- run_scraper.sh
+++ run_scraper.sh
@@ -19,7 +19,7 @@ python3 ./Scraper.py --term=$TERM >> $LOGFILE
 
 # parse the search result pages into more useful json
 echo "Parsing $TERM..."
-python3 ./Parser.py  >> $LOGFILE
+python3 ./Parser.py >> $LOGFILE
 
 # upload changes to mongodb MONGODB_STR should be set in system environment variables
 for filename in ./json/*.json; do
